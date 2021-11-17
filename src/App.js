5179454631b7bd1028e2bfe81f835b91f5d42da3
mehedi23 <satusatusatu0@gmail.com>
2021-11-17 19:15:26 +0600

    post css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="md:p-6 h-40 p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 hover:bg-gray-100 transition duration-500 hover:shadow-none">
+            <div className="flex-shrink-0">
+                {/* <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"> */}
+            </div>
+            <div>
+                <div className="text-xl font-medium text-black">ChitChat</div>
+                <p className="text-gray-500">You have a new message!</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
